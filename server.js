b89c23cad3891df81a08517da72e2a76de9de676
weintrsh <wein.sharon@gmail.com>
2024-05-26 13:33:05 +0300

    delete

--- server.js
+++ server.js
@@ -41,6 +41,31 @@ app.get('/api/tasks', async (req, res,next) => {
     console.error('Error reading or validating tasks:', error);
     res.status(500).json({ message: 'Internal Server Error,' + error });
   }
+
+  app.delete('/api/tasks/:taskId', async (req,res)=>
+    {
+      try
+      {
+        const taskId = req.params.taskId;
+        let tasks = await readTasks();
+        const taskIndex = tasks.findIndex((task)=>task.id===taskId);
+        if(taskIndex!=-1)
+        {
+          tasks.splice(taskIndex,1);
+          await writeTasks(tasks);
+          res.status(200).json({message:`task ${taskId} was deleted`});
+        }
+        else
+        {
+          res.status(404).json({message: 'task not found'});
+        }
+      }
+      catch(err)
+      {
+        res.status(500).json({message: err.message});
+      }
+    }
+  );
   // const error = new Error('Something went wrong');
   // error.status = 500; // Set custom status code
   // if(error)  
